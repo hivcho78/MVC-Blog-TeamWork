ad9cb238742e664d19f0ec0afed4e75e7932b89a
Ivailo Dimitrov/softuni <hivcho78@abv.bg>
2016-08-21 21:45:10 +0300

    first
    Beginning

---
+++ MVC-Blog-TeamWork/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVC_Blog_TeamWork.MvcApplication" Language="C#" %>
---
+++ MVC-Blog-TeamWork/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVC_Blog_TeamWork.Startup))]
+namespace MVC_Blog_TeamWork
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
